ce4b069acb57ef1f8c59f8589bf544b732bae48e
FrancescoDeidda <deidda.francesco8@gmail.com>
2023-11-09 11:38:06 +0100

    Commit 9 Novembre 2023 Esercitazione 4 funzionante

--- Esercitazioni /Esercitazione 4/Esercizio10.ml
+++ Esercitazioni /Esercitazione 4/Esercizio10.ml
@@ -1,9 +1,9 @@
 let consecutive_even l =
-  let rec count n l = match l with
-  (n, []) -> 0
-  |(n, l1::l2::l3) when ((l1 mod 2) = 0) && ((l2 mod 2) = 0) -> count n+1 (l2::l3)
-  |(n, l1::l2::l3) when ((l1 mod 2) != 0) || ((l2 mod 2) != 0) -> count 0 (l2::l3)
-in count 0 l
+  let rec count n max l'= match (n, l') with
+  (n, []) -> max
+  |(n, l1::l2) when (l1 mod 2) = 0 -> if(max > n) then count (n+1) max l2 else count (n+1) (n+1) l2
+  |(n, l1::l2) when (l1 mod 2) <> 0 -> count 0 max l2
+in count 0 0 l
 ;;
 
 assert(consecutive_even [] = 0);;
@@ -11,5 +11,3 @@ assert(consecutive_even [] = 0);;
 assert(consecutive_even [1;2;3;4;5;6] = 1);; 
 
 assert(consecutive_even [1;2;2;3;4;5] = 2);;
-
-;;
